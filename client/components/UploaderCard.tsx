73e5d5d0b3a622c584ba757ac1e14741b4066683
JuanDls01 <juanignaciodelossantos01@gmail.com>
2022-08-31 20:30:01 -0300

    Iniciando proyecto

---
+++ client/components/UploaderCard.tsx
@@ -0,0 +1,13 @@
+import {FcAddImage} from 'react-icons/fc'
+
+export const UploaderCard = () => {
+    return (
+        <div>
+            <h2>Upload your image</h2>
+            <p>Fiile should be Jpeg, Png...</p>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
