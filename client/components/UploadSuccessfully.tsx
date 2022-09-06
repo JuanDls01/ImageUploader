04b717a6750048ad5fefac5d396b32446c143c77
JuanDls01 <juanignaciodelossantos01@gmail.com>
2022-09-06 07:35:49 -0300

    Subniendo imagenes

--- client/components/UploadSuccessfully.tsx
+++ client/components/UploadSuccessfully.tsx
@@ -11,8 +11,8 @@ export const UploadSuccessfully = ({imageUrl}: any) => {
         <>
             <FcOk className='w-10 h-10 m-3'/>
             <h2 className='font-sans font-medium text-xl '>Uploaded Successfully</h2>
-            <img src={fullImageUrl} className='rounded-2xl w-80 h-52 m-8'/>
-            <div className='flex mb-3 rounded-lg bg-blue-100 w-80 h-10 p-1'>
+            <img src={fullImageUrl} className='rounded-2xl max-w-80 max-h-52 m-8'/>
+            <div className='flex mb-3 rounded-lg bg-blue-100 w-5/6 h-10 p-1'>
                 <input type='text' defaultValue={fullImageUrl} readOnly className='bg-transparent text-black w-80'></input>
                 <button onClick={copyHandler} className='bg-sky-500 hover:bg-sky-700 py-1 px-2 rounded-md flex items-center justify-center text-sm text-white'>Copy</button>
             </div>
